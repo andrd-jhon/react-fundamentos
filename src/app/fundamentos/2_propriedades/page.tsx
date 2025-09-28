e95c33b11efe795f5d8d2e793462e8a1a2f3230a
andrd-jhon <jv-andrade02@outlook.com>
2025-09-28 01:29:04 -0300

    Utilizando componentes em formato de tags.

---
+++ src/app/fundamentos/2_propriedades/page.tsx
@@ -0,0 +1,13 @@
+import { Acomodacao, Cabecalho, Menu } from "@/components/interface";
+
+export default function Pagina(){
+    return (
+        <div>
+        <Cabecalho/>
+        <Menu/>
+        <hr />
+        <Acomodacao/>
+
+    </div>
+    )
+}
