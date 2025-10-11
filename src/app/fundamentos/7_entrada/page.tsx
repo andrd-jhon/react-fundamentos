040a1a80ddd8833f795cc9593d4f68c61f66593a
andrd-jhon <jv-andrade02@outlook.com>
2025-10-11 15:09:16 -0300

    Criando caixa de texto e aplicando estilos do Tailwind.

---
+++ src/app/fundamentos/7_entrada/page.tsx
@@ -0,0 +1,12 @@
+export default function Entrada(){
+    return (
+        <div className="p-2">
+            <h1>Entrada de Dados</h1>
+            <input 
+                className="campo-texto"
+                type="text" 
+                placeholder="texto" 
+            />
+        </div>
+    )
+}
