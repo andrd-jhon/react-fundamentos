aa0a5fa20ea5b7d40d0aaeada662e59fd0697882
andrd-jhon <jv-andrade02@outlook.com>
2025-10-11 14:30:00 -0300

    Utilizando App com pages.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../app/globals.css'
+
+export default function App(props: any){
+    console.log(props)
+
+    const { Component, pageProps } = props
+    return <Component {...pageProps}/>
+}
